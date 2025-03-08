b7c6bb9d8d0e9e0aecfbe52c6e3e73232f08163a
abdorrahamni <mohammad02abdorrahmani@gmail.com>
2025-03-08 13:45:40 +0330

    ✨ feat: implement command-line interface for Gophel application manager

--- main.go
+++ main.go
@@ -2,12 +2,32 @@ package Gophel
 
 import (
 	"fmt"
+	"github.com/abdorrahmani/gophel/cmd"
+	"github.com/spf13/cobra"
+	"os"
 	"os/exec"
 )
 
 func main() {
 	// Check and install Go if not present
 	checkGoInstallation()
+
+	rootCmd := &cobra.Command{
+		Use:   "gophel",
+		Short: "Gophel - Go Application Manager",
+	}
+	rootCmd.AddCommand(cmd.BuildCmd)
+	rootCmd.AddCommand(cmd.RebuildCmd)
+	rootCmd.AddCommand(cmd.StartCmd)
+	rootCmd.AddCommand(cmd.RestartCmd)
+	rootCmd.AddCommand(cmd.StatusCmd)
+	rootCmd.AddCommand(cmd.StopCmd)
+	rootCmd.AddCommand(cmd.ListCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func checkGoInstallation() {
