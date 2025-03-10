b284dad7dcc2ce6b2e8965c2ea2c136291c592d2
abdorrahamni <mohammad02abdorrahmani@gmail.com>
2025-03-10 12:02:39 +0330

    💚 build: add installation script for gophel

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Exit immediately if a commend exists with a non-zero status
+set -e
+
+echo "Building gophel..."
+
+go build -o gophel
+
+echo "Moving gophel to /usr/local/bin/..."
+sudo mv gophel /usr/lcoal/bin/
+
+echo "Setting executable permission"
+sudo chmod +x /usr/local/bin/gophel
+
+echo "✅ Installation completed! You can now run 'gophel --help'."
